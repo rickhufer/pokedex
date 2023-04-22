837a0b8b4ed822c5cfb3bba7ff76dc27b2e75dd8
Ricardo <rickhufer@outlook.com>
2023-04-22 02:36:55 -0500

    form avanzado

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Search from './components/Search/Search';
 import Landing from './components/Landing/Landing';
+import Form from './components/Form/Form';
 import Detail from './components/Detail/Detail';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/home/search" element={<Search />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Form />} />
         <Route path="/detail/:detailId" element={<Detail />} />
       </Routes>
     </div>
--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,68 +1,222 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import styles from "./Form.module.css"
 import validate from "./validate"
+import { allTypes } from "../../redux/actions";
 
 const Form = (props) => {
-  // const [form, setForm] = useState({
-  //   username: "",
-  //   password: "",
-  // });
-  // const [errors, setErrors] = useState({
-  //   username: "",
-  //   password: "",
-  // });
+  const myTypes = useSelector((state) => state.myTypes)
+  const dispatch = useDispatch();
 
-  // const handleinputChange = (event) => {
-  //   const prop = event.target.name;
-  //   const val = event.target.value;
+  useEffect(() => {
+    async function inicio() {
+      await dispatch(allTypes());
+    }
+    inicio();
+    console.log(myTypes);
+  }, []);
 
-  //   setForm({ ...form, [prop]: val, });
-  //   setErrors(validate({ ...form, [prop]: val, }, errors));
-  // }
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   props.login(form);
 
-  //   if (errors.username === "" && errors.password === "") {
-  //     alert("Bienvenido");
-  //     setErrors({ username: "", password: "" })
-  //     setForm({ username: "", password: "" })
-  //   }
-  //   else alert("Datos incorrectos")
-  // }
+  const [form, setForm] = useState({
+    name: "",
+    hp: 0,
+    attack: 0,
+    defense: 0,
+    speed: 0, //opcional
+    height: 0, //opcional
+    weight: 0, //opcional
+    image: "",
+    types: []
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    speed: "", //opcional
+    height: "", //opcional
+    weight: "", //opcional
+    image: "",
+    types: ""
+  });
 
-  // return (
-  //   <div className={styles.formContainer}>
-  //     <form onSubmit={handleSubmit}>
-  //       <h1>Bienvenidos</h1><hr /><br />
-  //       <div className={styles.group}>
-  //         <label className={styles.label} htmlFor="username" >Email: </label>
-  //         <div>
-  //           <input
-  //             type="text"
-  //             name='username'
-  //             className={errors.username ? styles.errors : styles.success}
-  //             value={form.username}
-  //             onChange={handleinputChange} />
-  //           <br /><span className={styles.inputMessage}>{errors.username && errors.username}</span>
-  //         </div>
-  //       </div>
-  //       <div className={styles.group}>
-  //         <label className={styles.label} htmlFor="password">Contraseña: </label>
-  //         <div>
-  //           <input
-  //             type="text"
-  //             name='password'
-  //             className={errors.password ? styles.errors : styles.success}
-  //             value={form.password}
-  //             onChange={handleinputChange} />
-  //           <br /><span className={styles.inputMessage}>{errors.password}</span>
-  //         </div>
-  //       </div>
-  //       <button type='submit' className={styles.button}>Iniciar sesión</button>
-  //     </form>
-  //   </div>
-  // );
+  const getTypeIndex = (type) => myTypes.indexOf(type) + 1;
+
+  const handleinputChange = (event) => {
+    const prop = event.target.name;
+    const val = event.target.value;
+    const isChecked = event.target.checked;
+
+    setForm({ ...form, [prop]: val, });
+    // setErrors(validate({ ...form, [prop]: val, }, errors));
+  }
+  const handleinputChangeCheck = (event) => {
+    const val = event.target.value;
+    const isChecked = event.target.checked;
+
+    setForm((prevState) => {
+      if (isChecked) {
+        console.log(isChecked);
+        return {
+          ...prevState,
+          types: [...prevState.types, getTypeIndex(val)]
+        };
+      } else {
+        return {
+          ...prevState,
+          types: prevState.types.filter((item) => item !== getTypeIndex(val))
+        };
+      }
+    });
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // props.login(form);
+    console.log(form);
+    if (errors.username === "" && errors.password === "") {
+      alert("El pokemon fue creado");
+      setErrors({ username: "", password: "" })
+      setForm({ username: "", password: "" })
+    }
+    else alert("Datos incorrectos")
+  }
+
+  async function inicio() {
+    await dispatch(allTypes());
+  }
+  inicio();
+
+  return (
+    <div className={styles.formContainer}>
+      <form onSubmit={handleSubmit}>
+        <h1>Crea tu propio pokemon</h1><hr /><br />
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="name" >Nombre: </label>
+          <div>
+            <input
+              type="text"
+              name='name'
+              className={errors.name ? styles.errors : styles.success}
+              value={form.name}
+              onChange={handleinputChange} />
+            <br /><span className={styles.inputMessage}>{errors.name}</span>
+          </div>
+        </div>
+
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="hp" >Vida: </label>
+          <div>
+            <input
+              type="number"
+              name='hp'
+              className={errors.hp ? styles.errors : styles.success}
+              value={form.hp}
+              onChange={handleinputChange} />
+            <br /><span className={styles.inputMessage}>{errors.hp}</span>
+          </div>
+        </div>
+
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="attack" >Ataque: </label>
+          <div>
+            <input
+              type="number"
+              name='attack'
+              className={errors.attack ? styles.errors : styles.success}
+              value={form.attack}
+              onChange={handleinputChange} />
+            <br /><span className={styles.inputMessage}>{errors.attack}</span>
+          </div>
+        </div>
+
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="defense" >Defensa: </label>
+          <div>
+            <input
+              type="number"
+              name='defense'
+              className={errors.defense ? styles.errors : styles.success}
+              value={form.defense}
+              onChange={handleinputChange} />
+            <br /><span className={styles.inputMessage}>{errors.defense}</span>
+          </div>
+        </div>
+
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="speed" >Velocidad: </label>
+          <div>
+            <input
+              type="number"
+              name='speed'
+              className={errors.speed ? styles.errors : styles.success}
+              value={form.speed}
+              onChange={handleinputChange} />
+            <br /><span className={styles.inputMessage}>{errors.speed}</span>
+          </div>
+        </div>
+
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="height" >Altura: </label>
+          <div>
+            <input
+              type="number"
+              name='height'
+              className={errors.height ? styles.errors : styles.success}
+              value={form.height}
+              onChange={handleinputChange} />
+            <br /><span className={styles.inputMessage}>{errors.height}</span>
+          </div>
+        </div>
+
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="weight" >Peso: </label>
+          <div>
+            <input
+              type="number"
+              name='weight'
+              className={errors.weight ? styles.errors : styles.success}
+              value={form.weight}
+              onChange={handleinputChange} />
+            <br /><span className={styles.inputMessage}>{errors.weight}</span>
+          </div>
+        </div>
+
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="types" >Tipos: </label>
+          <div>
+            {myTypes.map((type, index) => (
+              <label key={type}>
+                <input
+                  type="checkbox"
+                  value={type}
+                  checked={form.types.includes(getTypeIndex(type))}
+                  onChange={handleinputChangeCheck}
+                />
+                {type}
+              </label>
+            ))}
+            <br /><span className={styles.inputMessage}>{errors.types}</span>
+          </div>
+        </div>
+
+        <div className={styles.group}>
+          <label className={styles.label} htmlFor="image" >Sube una imagen: </label>
+          <div>
+            <input
+              type="text"
+              name='image'
+              className={errors.image ? styles.errors : styles.success}
+              value={form.image}
+              onChange={handleinputChange} />
+            <br /><span className={styles.inputMessage}>{errors.image}</span>
+          </div>
+        </div>
+
+
+        <button type='submit' className={styles.button}>Crear</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
