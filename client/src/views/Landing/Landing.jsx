14f51d87d41c39f925c13ae1270a16baf0efdcec
Ricardo <rickhufer@outlook.com>
2023-04-25 10:40:34 -0500

    algo más

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <div className={styles.container}>
-      <h1>POKÉMON</h1>
-      <Link to="/home"><button>Ingresar</button></Link>
+      {/* <h1>POKÉMON</h1> */}
+      <Link className={styles.logo} to="/home"><img className={styles.logoimg} src="../../assets/images/logo.png" alt="" /></Link>
+      <Link to="/home"><button className={styles.button}>Ingresar</button></Link>
     </div>
   )
 }
