c51896841988fa5ce285842307da4013b89e4d02
Ricardo Huallpayunca <rickhufer@outlook.com>
2023-07-27 18:16:55 -0500

    fix: client-logo

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png"
 
 import styles from "./Landing.module.css"
 
 const Landing = () => {
   return (
     <div className={styles.container}>
-      <Link className={styles.logo} to="/home"><img className={styles.logoimg} src="../../assets/images/logo.png" alt="" /></Link>
+      <Link className={styles.logo} to="/home"><img className={styles.logoimg} src={logo} alt="" /></Link>
       <Link to="/home"><button className={styles.button}>Ingresar</button></Link>
     </div>
   )
